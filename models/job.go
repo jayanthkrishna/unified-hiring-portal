42c3237945e780c7b3f1905a24d6a9324ae47ac2
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-09-17 23:29:08 +0530

    Added tags into models

--- models/job.go
+++ models/job.go
@@ -6,12 +6,18 @@ import (
 
 type Job struct {
 	gorm.Model
-	JobTitle    string `json:"name"`
-	Company     string `json:"company"`
-	Description string `json:"description"`
-	Position    string `json:"position"`
-	// ApplicantID uint        `gorm:"default:null" json:"-"`
-	Applicants []Applicant `json:"applicants" gorm:"many2many:job_applications;"`
-	EmployerID uint        `json:"employer_id"`
-	Employer   User        `gorm:"foreignKey:EmployerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	JobTitle    string      `json:"name" gorm:"not null;type:varchar(255)"`
+	Company     string      `json:"company" gorm:"not null;type:varchar(255)"`
+	Description string      `json:"description" gorm:"not null;type:varchar(10000)"`
+	Position    string      `json:"position"`
+	Tags        []Tag       `json:"tags" gorm:"many2many:job_tags;"`
+	Applicants  []Applicant `json:"applicants" gorm:"many2many:job_applications;"`
+	EmployerID  uint        `json:"employer_id"`
+	Employer    User        `gorm:"foreignKey:EmployerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+type Tag struct {
+	gorm.Model
+	Name string `json:"tag_name" gorm:"uniqueIndex;type:varchar(255)"`
+	Jobs []Job  `json:"jobs" gorm:"many2many:job_tags;"`
 }
