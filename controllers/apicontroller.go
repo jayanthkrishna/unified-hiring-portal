2626c6e20a5ad5b2436e8c7d35bdeda14d4c71b1
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-09-23 23:39:22 +0530

    Added Post User for Clients

--- controllers/apicontroller.go
+++ controllers/apicontroller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"net/http"
 	"unified-hiring-portal/database"
 	"unified-hiring-portal/models"
 
@@ -9,7 +10,52 @@ import (
 
 func AddJobApplicant(c *fiber.Ctx) error {
 
-	return nil
+	jobid := c.Params("jobid")
+
+	applicant := models.Applicant{}
+
+	err := c.BodyParser(&applicant)
+
+	if err != nil {
+		return c.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"Message": "Request failed. Unprocessed",
+		})
+	}
+
+	existingApplicant := models.Applicant{}
+	err = database.DB.Where("Email = ?", applicant.Email).First(&existingApplicant).Error
+
+	if err != nil {
+
+		err = database.DB.Create(&applicant).Error
+
+		if err != nil {
+			return c.JSON(fiber.Map{
+				"Error": "Could not create Applicant",
+			})
+		}
+	} else {
+		applicant.ID = existingApplicant.ID
+
+	}
+
+	application_object := map[string]interface{}{
+		"job_id":       jobid,
+		"applicant_id": applicant.ID,
+	}
+
+	err = database.DB.Table("job_applications").Create(&application_object).Error
+
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"Error": err,
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"Message": "Success",
+	})
+
 }
 
 func GetAllJobs(c *fiber.Ctx) error {
